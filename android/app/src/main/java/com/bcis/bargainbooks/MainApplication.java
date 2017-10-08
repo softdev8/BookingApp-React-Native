347b4e8ad83c1e068b6d6ea9e5d872b231e7ec92
CrazyHybrid <oncemoreforyou@outlook.com>
2017-10-08 13:57:29 +0800

    initial android

---
+++ android/app/src/main/java/com/bcis/bargainbooks/MainApplication.java
@@ -0,0 +1,63 @@
+package com.bcis.bargainbooks;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+
+import com.airbnb.android.react.maps.MapsPackage;
+
+
+
+import com.github.yamill.orientation.OrientationPackage;
+
+import com.evollu.react.fcm.FIRMessagingPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.calendarevents.CalendarEventsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.github.xinthink.rnmk.ReactMaterialKitPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+
+
+            new OrientationPackage(),            
+
+
+            new MapsPackage(),
+            new FIRMessagingPackage(),
+
+            new CalendarEventsPackage(),
+            new VectorIconsPackage(),
+            new ReactMaterialKitPackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);   
+  }
+}
