e1fa6f6f5014d16a9ec8684481e103695ba077dc
Mobile Developer <oncemoreforyou@outlook.com>
2018-10-18 23:20:34 -0400

    Update SideBarContainer.js

--- src/containers/SideBarContainer.js
+++ src/containers/SideBarContainer.js
@@ -8,30 +8,30 @@ import {logOutUser, closeTab} from '../actions';
 
 class SideBarContainer extends Component {
   handleLogOut() {
-		AsyncStorage.multiRemove(
-			['access_token', 'refresh_token', 'user_name', 'token_type'])
-			.then( ()=> {
-				this.props.logOutUser();
-				Actions.auth();
-			})
-			.catch((err) => Alert.alert(`Error name: ${err.name}`, `Error message: ${err.message}`));
-	}
+    AsyncStorage.multiRemove(
+      ['access_token', 'refresh_token', 'user_name', 'token_type'])
+	.then( ()=> {
+	  this.props.logOutUser();
+	  Actions.auth();
+	})
+	.catch((err) => Alert.alert(`Error name: ${err.name}`, `Error message: ${err.message}`));
+  }
 
-	render() {
-    		console.log('tab', this.props.tab);
-		return (
-			<SideBar {...this.props} onLogOut={this.handleLogOut.bind(this)}/>
-		);
-	}
+  render() {
+    console.log('tab', this.props.tab);
+    return (
+	<SideBar {...this.props} onLogOut={this.handleLogOut.bind(this)}/>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-	return {
-		authenticated: state.auth.authenticated,
-		user: state.auth.user,
-		profile: state.auth.profile,
+  return {
+    authenticated: state.auth.authenticated,
+    user: state.auth.user,
+    profile: state.auth.profile,
     tab: state.routes.tab,
-	};
+  };
 };
 
 
