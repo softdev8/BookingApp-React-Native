af4066f02117b574ad21bc53e764a02a6696ab15
Mobile Developer <oncemoreforyou@outlook.com>
2018-10-22 14:04:16 -0400

    Update authentication_reducer.js

--- src/reducers/authentication_reducer.js
+++ src/reducers/authentication_reducer.js
@@ -15,35 +15,35 @@ const INITIAL_STATE = {
 };
 
 export default (state = INITIAL_STATE, action) => {
-	switch (action.type) {
-		case AUTH_USER:
-			return {
-				...state,
-				authenticated: true,
-				user: action.payload,
-				token: 'Bearer ' + action.payload.access_token,
-				loading: false
-			};
-		case REGISTER_USER:
-			return {
-				...state,
-				// authenticated: true,
-				// user: action.payload,
-				loading: false
-			};
-		case FETCH_USER:
-			return {
-				...state,
-				profile: action.payload,
-			};
-		case UNAUTH_USER:
-			return INITIAL_STATE;
-		case CHANGE_EMAIL:
-			return {...state, user: action.payload};
-		case AUTH_REQUEST:
-			return {...state, loading: true};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case AUTH_USER:
+      return {
+	...state,
+	authenticated: true,
+	user: action.payload,
+	token: 'Bearer ' + action.payload.access_token,
+	loading: false
+    };
+    case REGISTER_USER:
+      return {
+	...state,
+	// authenticated: true,
+	// user: action.payload,
+	loading: false
+      };
+    case FETCH_USER:
+      return {
+	...state,
+	profile: action.payload,
+      };
+    case UNAUTH_USER:
+	return INITIAL_STATE;
+    case CHANGE_EMAIL:
+	return {...state, user: action.payload};
+    case AUTH_REQUEST:
+	return {...state, loading: true};
+    default:
+	return state;
+  }
 };
 
